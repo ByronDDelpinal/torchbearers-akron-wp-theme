6a904b192ee0d14512daa7907a1f1cdfb95da4df
Sean Blasko <sblasko4868@gmail.com>
2022-06-16 17:39:48 -0400

    Starting work

--- src/admin.js
+++ src/admin.js
@@ -1,1 +1,12 @@
 
+function setDefaultVirtualEventSettings() {
+    // Set button text to "Join Zoom Link"
+    document.querySelector("#tribe-events-virtual-virtual-button-text").value = "Join Zoom Link";
+    document.querySelector("#tribe-events-virtual-virtual-button-text").readonly = true;
+    document.querySelector("#tribe-events-virtual-virtual-button-text").onclick = "return false;"
+
+    // Set "Embed Video" checkbox to false
+    document.querySelector("#tribe-events-virtual-embed-video").checked = false;
+}
+
+document.querySelector("button.tribe-configure-virtual-button.button").addEventListener("click", setDefaultVirtualEventSettings);
