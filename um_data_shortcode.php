0c1ad2715e1fea8f3278716efa8ffd17f92b3697
Sean Blasko <sblasko4868@gmail.com>
2023-11-15 02:27:49 -0500

    Fixing comment section

--- um_data_shortcode.php
+++ um_data_shortcode.php
@@ -1,12 +1,13 @@
-<!-- 
-    Call shortcode and optionally specify an ultimate member field to retrieve
-
-        [userdata value='first_name'] : returns first_name of current user
-
-        [userdata] : returns member_id of current user
- -->
-
 <?php 
+
+/** 
+*    Call shortcode and optionally specify an ultimate member field to retrieve
+*
+*       [userdata value='first_name'] : returns first_name of current user
+*
+*        [userdata] : returns member_id of current user
+*/
+
 function get_user_data($atts) {
     return um_user(shortcode_atts( array (
         'value' => 'member_id'
